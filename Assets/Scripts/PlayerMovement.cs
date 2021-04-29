fcd469f0d2e3a4c497a322121b2f3c614160ea21
Alekander Bialik <aleksanderbialik97@gmail.com>
2021-04-29 23:40:06 +0200

    added player with basic animation and controllers

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float jumpHeight;
+	[SerializeField] private float gravity;
+	[SerializeField] private float groundCheckDistance;
+	[SerializeField] private LayerMask groundMask;
+	
+
+	private Vector3 moveDirection;
+	private Vector3 velocity;
+	
+
+	private CharacterController controller;
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		float moveZ = Input.GetAxis("Vertical");
+		float moveX = Input.GetAxis("Horizontal");
+
+		moveDirection = new Vector3(moveX, 0, moveZ);
+		moveDirection *= moveSpeed;
+		moveDirection = transform.TransformDirection(moveDirection);
+		if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			moveDirection.y = jumpHeight;
+		}
+		
+		
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+	
+}
---
+++ Assets/Scripts/RotateWithMouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+    [SerializeField] private float mouseSens;
+    private Transform player;
+
+    private void Start()
+    {
+        player = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        
+        player.Rotate(Vector3.up, mouseX);
+    }
+}
